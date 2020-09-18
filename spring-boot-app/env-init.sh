64fff04f34097b04b8211de2c86acb8d515cca3b
Josh Cummings <josh.cummings@gmail.com>
2020-09-18 10:58:30 -0600

    Attempt a Spring Boot Application

---
+++ spring-boot-app/env-init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Standard environment initialization script. Assumes the installation path (the cp portion) has been
+# created by Katacoda via a environment.uieditorpath key. (ex: "uieditorpath": "/root/code/spring-mvc")
+
+UI_PATH=/root/code 	# This should match your index.json key
+
+git clone -q https://github.com/jzheaux/oreilly-spring-security-rest-apis.git
+cd ${UI_PATH} && cp -R /root/oreilly-spring-security-rest-apis/* ./
+clear # To clean up Katacoda terminal noise
+mvn spring-boot:run
