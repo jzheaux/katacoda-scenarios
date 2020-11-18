27277c0365194e8c1bd12d9774e6a90b345af00a
Josh Cummings <josh.cummings@gmail.com>
2020-11-17 21:52:38 -0700

    Attempt to Make Terminals Open to /root/code

--- local-authentication/env-init.sh
+++ local-authentication/env-init.sh
@@ -4,6 +4,7 @@
 
 UI_PATH=/root/code 	# This should match your index.json key
 
+echo "cd $UI_PATH" >> ~/.bashrc
 git clone -q https://github.com/jzheaux/oreilly-spring-security-rest-apis.git
 rm -rf $UI_PATH && cp -R /root/oreilly-spring-security-rest-apis /root/code
 cd /root/code
